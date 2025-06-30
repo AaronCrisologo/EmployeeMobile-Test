dead2c8af0d7b63879432bbefc58bda6a4650592
AaronCrisologo <21-08166@g.batstate-u.edu.ph>
2025-06-30 14:22:36 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'EmployeeMobile-Test',
+  webDir: 'www'
+};
+
+export default config;
